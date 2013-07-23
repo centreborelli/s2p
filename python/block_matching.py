e61a501c36e0bef9c9f1e66300ecc566614b368c
Carlo De Franchis <carlodef@gmail.com>
2013-07-23 16:21:58 -0700

    add docstring

--- python/block_matching.py
+++ python/block_matching.py
@@ -11,6 +11,15 @@ hirshmuller = '%s/../3rdparty/stereo_hirschmuller_2002/subpix.sh' % (
 
 
 def compute_disparity_map(im1, im2, disp_range, out_disp, algo):
+    """
+    Runs a block-matching binary on a pair of stereo-rectified images.
+
+    Args:
+        im1, im2: rectified stereo pair
+        disp_range: path to a text file containing the disparity range
+        out_disp: path to the output file (diparity map)
+        algo: string used to indicate the desired binary
+    """
     # read the disp_range file
     disp = np.loadtxt(disp_range)
     disp_min = disp[0]
