dd1edbf39160828b426b88151c34484699944def
Carlo De Franchis <carlodef@gmail.com>
2013-08-08 15:44:10 -0700

    updated docstrings in wrapper callSGBM.sh

--- 3rdparty/stereo_hirschmuller_2008/callSGBM.sh
+++ 3rdparty/stereo_hirschmuller_2008/callSGBM.sh
@@ -5,13 +5,16 @@ rel_path_script=`dirname $0`
 
 if [ "$3" == "" ]; then
    echo "Usage:"
-   echo "  $0 im1 im2 out_disp out_mask [mindisp(0) maxdisp(60) LoG(0) regionRadius(3) maxPerPixelError(20) validateRtoL(1) texture(0.2)]"
+   echo "  $0 im1 im2 out_disp out_mask mindisp maxdisp win P1 P2 lr"
    echo ""
-   echo "  LoG: Laplacian of Gaussian preprocess 1:enabled 0:disabled"
-   echo "  regionRadius: radius of the window"
-   echo "  maxPerPixelError: <0 disabled"
-   echo "  validateRtoL: 1:enabled 0:disabled"
-   echo "  texture: normalized diffrence between best and second best minimum  0:disabled"
+   echo "  win: matched block size. It must be an odd number >=1."
+   echo "  P1: The first parameter controlling the disparity smoothness."
+   echo "  P2: The second parameter controlling the disparity smoothness. The"
+   echo "    larger the values are, the smoother the disparity is. P1 is the penalty on"
+   echo "    the disparity change by plus or minus 1 between neighbor pixels. P2 is the"
+   echo "    penalty on the disparity change by more than 1 between neighbor pixels. The"
+   echo "    algorithm requires P2 > P1"
+   echo "  lr: max allowed difference in the left-right disparity check."
    echo ""
    echo "  Wrapper to opencv SGBM function, which implements a modified version"
    echo "  of Hirschmuller's Semi-Global Matching (SGM):"
@@ -26,7 +29,6 @@ disp=$3
 mask=$4
 im=$5
 iM=$6
-
 SAD_win=$7
 P1=$8
 P2=$9
@@ -48,13 +50,11 @@ if [ $a_extension == "tif" ]; then
     b=/tmp/$b_name.png
 
     # HACK! make it invariant to illumination changes?
-    gblur 2 $a   | plambda  - " x 4 *    x(-1,0)  -1 *  +  x(1,0)  -1 * +  x(0,-1)  -1 * +    x(0,1)  -1 * + " | qauto - $a
-    gblur 2 $b   | plambda  - " x 4 *    x(-1,0)  -1 *  +  x(1,0)  -1 * +  x(0,-1)  -1 * +    x(0,1)  -1 * + " | qauto - $b
+    gblur 2 $a | plambda - " x 4 *    x(-1,0)  -1 *  +  x(1,0)  -1 * +  x(0,-1)  -1 * +    x(0,1)  -1 * + " | qauto - $a
+    gblur 2 $b | plambda - " x 4 *    x(-1,0)  -1 *  +  x(1,0)  -1 * +  x(0,-1)  -1 * +    x(0,1)  -1 * + " | qauto - $b
 
 fi
 
 #usage: ./build/SGBM im1 im2 out [mindisp(0) maxdisp(64) SADwindow(1) P1(0) P2(0) LRdiff(1)]
-$rel_path_script/SGBM $a $b $disp $im $iM $SAD_win $P1 $P2 $lr
+$rel_path_script/build/SGBM $a $b $disp $im $iM $SAD_win $P1 $P2 $lr
 plambda $disp "x isnan 0 255 if" | iion - $mask
-
-rm $a $b
