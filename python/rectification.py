[IMPORTANT]: fixed a bug in homography_cropper.py, affecting the precision of the rectification process. The computed homographies were correct, but incorrectly applied to images. To be applied, each homography is decomposed in a translation + homography. The translation part is applied by doing a crop, then the remaining homography is applied with the 'homography' binary, on the previous crop. There was an issue with rounding the coordinates of the crop.

python/rectification.py
@@ -179,6 +179,10 @@ def register_horizontally(matches, H1, H2, do_shear=True, flag='center'):
     x2 = pt2[:, 0]
     y2 = pt2[:, 1]
 
+    # for debug, print the vertical disparities. Should be zero.
+    print "Residual vertical disparities: max, min, mean. Should be zero ------"
+    print np.max(y2 - y1), np.min(y2 - y1), np.mean(y2 - y1)
+
     # shear correction
     # we search the (s, b) vector that minimises \sum (x1 - (x2+s*y2+b))^2
     # it is a least squares minimisation problem
@@ -223,9 +227,6 @@ def register_horizontally(matches, H1, H2, do_shear=True, flag='center'):
     else:
         dispx_max = (1-d) * dispx_max
 
-    # for debug, print the vertical disparities. Should be zero.
-    print "Residual vertical disparities: min, max, mean. Should be zero ------"
-    print np.min(y2 - y1), np.max(y2 - y1), np.mean(y2 - y1)
     return H2, dispx_min, dispx_max
 
 
@@ -341,13 +342,13 @@ def compute_rectification_homographies(im1, im2, rpc1, rpc2, x, y, w, h, A=None)
     H2 = np.dot(H2, T2)
 
     # for debug
-    print "min, max, mean rectification error on rpc matches ------------------"
+    print "max, min, mean rectification error on rpc matches ------------------"
     tmp = common.points_apply_homography(H1, p1)
     y1 = tmp[:, 1]
     tmp = common.points_apply_homography(H2, p2)
     y2 = tmp[:, 1]
     err = np.abs(y1 - y2)
-    print np.min(err), np.max(err), np.mean(err)
+    print np.max(err), np.min(err), np.mean(err)
 
     print "step 4: pull back top-left corner of the ROI in the origin ---------"
     roi = [[x, y], [x+w, y], [x+w, y+h], [x, y+h]]
@@ -376,6 +377,7 @@ def compute_rectification_homographies(im1, im2, rpc1, rpc2, x, y, w, h, A=None)
     sift_matches = filter_matches_epipolar_constraint(F, sift_matches,
         epipolar_thresh)
     visualisation.plot_matches_pleiades(im1, im2, sift_matches)
+    print 'remaining sift matches', len(sift_matches)
     if not len(sift_matches):
         print """all the sift matches have been discarded by the epipolar
         constraint. This is probably due to the pointing error. Try with a
@@ -386,7 +388,7 @@ def compute_rectification_homographies(im1, im2, rpc1, rpc2, x, y, w, h, A=None)
     disp_m, disp_M = update_minmax_range_extrapolating_registration_affinity(sift_matches,
         H1, H2, w, h)
 
-    return H1, H2, disp_m, disp_M
+    return H1, H2, disp_m, disp_M, sift_matches
 
 
 def rectify_pair(im1, im2, rpc1, rpc2, x, y, w, h, out1, out2, A=None):
@@ -420,7 +422,7 @@ def rectify_pair(im1, im2, rpc1, rpc2, x, y, w, h, out1, out2, A=None):
     rpc2 = rpc_model.RPCModel(rpc2)
 
     # compute rectifying homographies
-    H1, H2, disp_min, disp_max = compute_rectification_homographies(im1, im2,
+    H1, H2, disp_min, disp_max, matches = compute_rectification_homographies(im1, im2,
         rpc1, rpc2, x, y, w, h, A)
 
     # compute output images size
@@ -441,6 +443,13 @@ def rectify_pair(im1, im2, rpc1, rpc2, x, y, w, h, out1, out2, A=None):
     # apply homographies and do the crops
     homography_cropper.crop_and_apply_homography(out1, im1, H1, w0, h0, subsampling_factor)
     homography_cropper.crop_and_apply_homography(out2, im2, H2, w0, h0, subsampling_factor)
+
+    # plot the matches
+    # transform the matches according to the homographies
+    pt1 = common.points_apply_homography(H1, matches[:, 0:2])
+    pt2 = common.points_apply_homography(H2, matches[:, 2:4])
+    to_show = visualisation.plot_matches(out1, out2, np.hstack((pt1, pt2)))
+    common.run('v %s' % to_show)
 
     #  If subsampling_factor the homographies are altered to reflect the zoom
     if subsampling_factor != 1:

python/visualisation.py
@@ -9,7 +9,7 @@ def plot_line(im, x1, y1, x2, y2, colour):
     Args:
         im: 3D numpy array containing the image values. It may be stored as
             uint8 or float32.
-        x1, y1, x2, y2: coordinates of the line endpoints
+        x1, y1, x2, y2: integer coordinates of the line endpoints
         colour: list of length 3 giving the colour used for the plotted line
             (ie [r, g, b])
 
@@ -17,9 +17,6 @@ def plot_line(im, x1, y1, x2, y2, colour):
         a copy of the input numpy array, with the plotted lines on it. It means
         that the intensities of pixels located on the plotted line are changed.
     """
-    # convert endpoints to int
-    x1, y1, x2, y2 = np.round(x1), np.round(y1), np.round(x2), np.round(y2)
-
     # colour points of the line pixel by pixel. Loop over x or y, depending on
     # the biggest dimension.
     if np.abs(x2 - x1) >= np.abs(y2 - y1):
@@ -65,8 +62,8 @@ def plot_matches(im1, im2, matches):
     out[:h2, w1:w] = img2
 
     # define colors, according to min/max intensity values
-    out_min = min(np.min(img1), np.min(img2))
-    out_max = max(np.max(img1), np.max(img2))
+    out_min = min(np.nanmin(img1), np.nanmin(img2))
+    out_max = max(np.nanmax(img1), np.nanmax(img2))
     green = [out_min, out_max, out_min]
     blue = [out_min, out_min, out_max]
 
@@ -76,6 +73,8 @@ def plot_matches(im1, im2, matches):
         y1 = matches[i, 1]
         x2 = matches[i, 2] + w1
         y2 = matches[i, 3]
+        # convert endpoints to int (nn interpolation)
+        x1, y1, x2, y2 = np.round([x1, y1, x2, y2])
         plot_line(out, x1, y1, x2, y2, blue)
         out[y1, x1] = green
         out[y2, x2] = green
@@ -110,15 +109,16 @@ def plot_matches_pleiades(im1, im2, matches):
     y2 = np.min(matches[:, 3])
     h2 = np.max(matches[:, 3]) - y2
 
-    # add 20 pixels offset
-    x1 -= 20
-    y1 -= 20
-    x2 -= 20
-    y2 -= 20
-    w1 += 40
-    h1 += 40
-    w2 += 40
-    h2 += 40
+    # add 20 pixels offset and round. The image_crop_TIFF function will round
+    # off the coordinates before it does the crops.
+    x1 -= 20; x1 = np.round(x1)
+    y1 -= 20; y1 = np.round(y1)
+    x2 -= 20; x2 = np.round(x2)
+    y2 -= 20; y2 = np.round(y2)
+    w1 += 40; w1 = np.round(w1)
+    h1 += 40; h1 = np.round(h1)
+    w2 += 40; w2 = np.round(w2)
+    h2 += 40; h2 = np.round(h2)
 
     # do the crops
     crop1 = common.image_crop_TIFF(im1, x1, y1, w1, h1)