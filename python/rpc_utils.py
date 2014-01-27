a0220f54f84074ce5c58fe51f3169a6e6830d6d2
Julien Michel <julien.michel@orfeo-toolbox.org>
2014-01-27 14:38:02 +0000

    Adding a warning when out of srtm domain

--- python/rpc_utils.py
+++ python/rpc_utils.py
@@ -267,6 +267,7 @@ def altitude_range(rpc, x, y, w, h):
 
     # if bounding box is out of srtm domain, return coarse altitude estimation
     if (lat_m < -60 or lat_M > 60):
+        print "Out of SRTM domain, returning coarse range from rpc"
         return altitude_range_coarse(rpc)
 
     # sample the bounding box with regular step of 3 arcseconds (srtm
