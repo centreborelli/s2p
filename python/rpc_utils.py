79ab82d64f77a02cfd7d2d3d56ca9d90ba49cb87
Carlo De Franchis <carlodef@gmail.com>
2014-02-01 19:04:42 +0100

    indent

--- python/rpc_utils.py
+++ python/rpc_utils.py
@@ -68,32 +68,29 @@ def compute_height(model_a, model_b, x1, y1, x2, y2):
         r1 = np.vstack([tx,ty]).T
         a = r1 - r0
         b = p2 - r0
-        # implements: h0_inc = dot(a,b) / dot(a,a) For some reason,
-        # the formulation bellow causes massive memory leaks on some
-        # systems.
+        # implements: h0_inc = dot(a,b) / dot(a,a)
+        # For some reason, the formulation below causes massive memory leaks on
+        # some systems.
         # h0_inc = np.divide(np.diag(np.dot(a, b.T)), np.diag(np.dot(a, a.T)))
         # Replacing with the equivalent:
-        diagabdot = np.multiply(a[:,0],b[:,0])+np.multiply(a[:,1],b[:,1])
-        diagaadot = np.multiply(a[:,0],a[:,0])+np.multiply(a[:,1],a[:,1])
-        h0_inc = np.divide(diagabdot,diagaadot)
+        diagabdot = np.multiply(a[:, 0], b[:, 0]) + np.multiply(a[:, 1], b[:, 1])
+        diagaadot = np.multiply(a[:, 0], a[:, 0]) + np.multiply(a[:, 1], a[:, 1])
+        h0_inc = np.divide(diagabdot, diagaadot)
 #        if np.any(np.isnan(h0_inc)):
 #            print x1, y1, x2, y2
 #            print a
 #            return h0, h0*0
         # implements:   q = r0 + h0_inc * a
         q = r0 + np.dot(np.diag(h0_inc), a)
-        # implements: err = sqrt( dot(q-p2,q-p2) )
+        # implements: err = sqrt(dot(q-p2, q-p2))
         tmp = q-p2
-        err =  np.sqrt(np.multiply(tmp[:,0], tmp[:,0])+np.multiply(tmp[:,1], tmp[:,1]))
+        err =  np.sqrt(np.multiply(tmp[:, 0], tmp[:, 0]) + np.multiply(tmp[:, 1], tmp[:, 1]))
 #       print np.arctan2(tmp[:, 1], tmp[:, 0]) # for debug
 #       print err # for debug
-        h0 = np.add(h0,h0_inc*HSTEP)
+        h0 = np.add(h0, h0_inc*HSTEP)
         # implements: if fabs(h0_inc) < 0.0001:
         if np.max(np.fabs(h0_inc)) < 0.001:
             break
-
-
-
 
     return (h0, err)
 
