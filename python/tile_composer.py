2e17279666365bc0e1187ad1a8e3960f116ae57d
Julien Michel <julien.michel@orfeo-toolbox.org>
2014-03-05 13:13:00 +0000

    Fixing bugs on gdal tile composer

--- python/tile_composer.py
+++ python/tile_composer.py
@@ -25,32 +25,32 @@ def mosaic_gdal(fout, w, h, list_tiles, tw, th, ov):
     nty = np.ceil(float(h - ov) / (th - ov)).astype(int)
     assert(ntx * nty == N)
 
-    vrtfilename = common.tmpfile('.vrt')
+    vrtfilename = fout+'.vrt'
     
     vrtfile = open(vrtfilename,'w')
 
-    vrtfile.write("<VRTDataset rasterXSize=\"%i\" rasterYSize=\"%i\">" %(w,h))
-    vrtfile.write("\t<VRTRasterBand dataType=\"UInt32\" band=\"1\">")
-    vrtfile.write("\t\t<ColorInterp>Gray</ColorInterp>")
+    vrtfile.write("<VRTDataset rasterXSize=\"%i\" rasterYSize=\"%i\">\n" %(w,h))
+    vrtfile.write("\t<VRTRasterBand dataType=\"Float32\" band=\"1\">\n")
+    vrtfile.write("\t\t<ColorInterp>Gray</ColorInterp>\n")
 
     # loop over all the tiles
     for j in range(nty):
         for i in range(ntx):
             x0 = i * (tw - ov)
             y0 = j * (th - ov)
-            x1 = min(x0 + tw, w)
-            y1 = min(y0 + th, h)
+            x1 = min(x0 + tw -ov, w)
+            y1 = min(y0 + th - ov, h)
             tile_fname = list_tiles[j * ntx + i]
             if os.path.isfile(tile_fname):
-                vrtfile.write("\t\t<SimpleSource>")
-                vrtfile.write("\t\t\t<SourceFilename relativeToVRT=\"1\">%s</SourceFilename>" %tile_fname)
-                vrtfile.write("\t\t\t<SourceBand>1</SourceBand>")
-                vrtfile.write("\t\t\t<SrcRect xOff=\"%i\" yOff=\"%i\" xSize=\"%i\" ySize=\"%i\"/>" %(0,x1-x0,0,y1-y0))
-                vrtfile.write("\t\t\t<DstRect xOff=\"%i\" yOff=\"%i\" xSize=\"%i\" ySize=\"%i\"/>" %(x0,x1-x0,y0,y1-y0))
-                vrtfile.write("\t\t</SimpleSource>")
+                vrtfile.write("\t\t<SimpleSource>\n")
+                vrtfile.write("\t\t\t<SourceFilename relativeToVRT=\"1\">%s</SourceFilename>\n" %tile_fname)
+                vrtfile.write("\t\t\t<SourceBand>1</SourceBand>\n")
+                vrtfile.write("\t\t\t<SrcRect xOff=\"%i\" yOff=\"%i\" xSize=\"%i\" ySize=\"%i\"/>\n" %(0,0,x1-x0,y1-y0))
+                vrtfile.write("\t\t\t<DstRect xOff=\"%i\" yOff=\"%i\" xSize=\"%i\" ySize=\"%i\"/>\n" %(x0,y0,x1-x0,y1-y0))
+                vrtfile.write("\t\t</SimpleSource>\n")
     
-    vrtfile.write("\t</VRTRasterBand>")
-    vrtfile.write("</VRTDataset>")
+    vrtfile.write("\t</VRTRasterBand>\n")
+    vrtfile.write("</VRTDataset>\n")
     vrtfile.close()
 
     common.run('gdal_translate %s %s' %(vrtfilename,fout))
