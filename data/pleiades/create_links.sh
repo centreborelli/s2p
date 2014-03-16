871d0d20d00ffb5a50c3971895dc36660ecce7e1
Carlo De Franchis <carlodef@gmail.com>
2014-03-16 21:11:12 +0100

    fix sorting problem in create_links.sh script

--- data/pleiades/create_links.sh
+++ data/pleiades/create_links.sh
@@ -72,13 +72,13 @@ for f in $pleiades_dir/*; do
             # the dataset has subdatasets (multidate)
             for ff in $f/dataset_*; do
                 mkdir -p `basename $f`/`basename $ff`
-                find $ff | grep "JP2.TIF" | grep -v "JP2.TIF.ovr" | grep "_P_" >  `basename $f`/`basename $ff`/paths_panchro.txt
-                find $ff | grep "JP2.TIF" | grep -v "JP2.TIF.ovr" | grep "_MS_" > `basename $f`/`basename $ff`/paths_ms.txt
+                find $ff | grep "JP2.TIF" | grep -v "JP2.TIF.ovr" | grep "_P_" | sort >  `basename $f`/`basename $ff`/paths_panchro.txt
+                find $ff | grep "JP2.TIF" | grep -v "JP2.TIF.ovr" | grep "_MS_" | sort > `basename $f`/`basename $ff`/paths_ms.txt
             done
         else
             # the dataset has no subdatasets
-            find $f | grep "JP2.TIF" | grep -v "JP2.TIF.ovr" | grep "_P_" >  `basename $f`/paths_panchro.txt
-            find $f | grep "JP2.TIF" | grep -v "JP2.TIF.ovr" | grep "_MS_" > `basename $f`/paths_ms.txt
+            find $f | grep "JP2.TIF" | grep -v "JP2.TIF.ovr" | grep "_P_" | sort >  `basename $f`/paths_panchro.txt
+            find $f | grep "JP2.TIF" | grep -v "JP2.TIF.ovr" | grep "_MS_" | sort > `basename $f`/paths_ms.txt
         fi
     fi
 done
