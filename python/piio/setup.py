2547a0e2186da83bb52f412a4f093240aac192ad
Carlo De Franchis <carlodef@gmail.com>
2014-05-15 16:20:29 +0200

    removed -arch i386 from compiler preargs for Darwin (piio)

--- python/piio/setup.py
+++ python/piio/setup.py
@@ -7,35 +7,35 @@ from distutils.core import setup #, Extension
 
 class build_iio(distutils.cmd.Command):
     description = """build iio to a shared library"""
-    
+
     user_options = [('compiler=', 'c', 'specify the compiler type. It must understand GCC arguments')
                     ,('release', 'r', 'build iio without debug asserts')
                     ]
-    
+
     boolean_options = ['release']
-    
+
     help_options = [
         ('help-compiler', None, "list available compilers", cc.show_compilers)
         ]
 
-    compiler = None  
+    compiler = None
 
       ### libraries for IIO
     libraries = ['png','jpeg','tiff']
-        
+
     def initialize_options (self):
         self.compiler = None
         self.release = True
-        
+
     def finalize_options (self):
         pass
-    
+
     def compile(self):
         if self.release:
             print("compiling iio in Release mode (No debug output or asserts)" )
         else:
             print("compiling iio in Debug mode (Defualt, prints debug output and asserts)")
-        
+
         compiler = cc.new_compiler(compiler=self.compiler)
 
          # sources for IIO
@@ -53,46 +53,46 @@ class build_iio(distutils.cmd.Command):
         #            os.remove(fn_path)
 
         include_folders = source_folders
-        
-        compiler_preargs = ['-std=gnu99'] 
-        
+
+        compiler_preargs = ['-std=gnu99']
+
         if self.release:
             compiler_preargs.append('-DNDEBUG')
-        
+
         # check if we are on a 64bit python
         arch = ctypes.sizeof(ctypes.c_voidp) * 8
-        
+
         if arch == 64 and platform.system() == 'Linux':
             compiler_preargs += ['-m64', '-O3', '-fPIC']
         elif arch == 32 and platform.system() == 'Linux':
             compiler_preargs += ['-m32', '-O3']
         elif platform.system() == 'Darwin':
             #No -O3 on OSX. There's a bug in the clang compiler when using O3.
-            compiler_preargs += ['-O3', '-arch', 'i386', '-arch', 'x86_64']
-        
+            compiler_preargs += ['-O3', '-arch', 'x86_64']
+
         if platform.system() in ('Windows', 'Microsoft'):
-            # Compile with stddecl instead of cdecl (-mrtd). 
+            # Compile with stddecl instead of cdecl (-mrtd).
             # Using cdecl cause a missing bytes issue in some cases
             # Because -mrtd and -fomit-frame-pointer (which is included in -O)
             # gives problem with function pointer to the sdtlib free function
             # we also have to use -fno-omit-frame-pointer
-            compiler_preargs += ['-mrtd', '-O3', '-shared', '-fno-omit-frame-pointer'] 
-        
+            compiler_preargs += ['-mrtd', '-O3', '-shared', '-fno-omit-frame-pointer']
+
         if arch == 64 and platform.system() in ('Windows', 'Microsoft'):
             compiler_preargs += ['-m64']
         if arch == 32 and platform.system() in ('Windows', 'Microsoft'):
             compiler_preargs += ['-m32']
-            
+
         for x in compiler.executables:
             args = getattr(compiler, x)
             try:
-                args.remove('-mno-cygwin') #Not available on newer versions of gcc 
+                args.remove('-mno-cygwin') #Not available on newer versions of gcc
                 args.remove('-mdll')
             except:
                 pass
-        
+
         objs = compiler.compile(sources, include_dirs=include_folders, extra_preargs=compiler_preargs)
-        
+
         libname = 'iio'
         if arch == 64 and platform.system() != 'Darwin':
             libname += ''
@@ -106,14 +106,14 @@ class build_iio(distutils.cmd.Command):
             linker_preargs += ['-fPIC']
         if platform.system() in ('Windows', 'Microsoft'):
             # link with stddecl instead of cdecl
-            linker_preargs += ['-mrtd'] 
+            linker_preargs += ['-mrtd']
             # remove link against msvcr*. this is a bit ugly maybe.. :)
             compiler.dll_libraries = [lib for lib in compiler.dll_libraries if not lib.startswith("msvcr")]
         compiler.link(cc.CCompiler.SHARED_LIBRARY, objs, libname, output_dir = './', extra_preargs=linker_preargs)
-        
+
     def run(self):
         self.compile()
-        
+
 
 
 setup(
