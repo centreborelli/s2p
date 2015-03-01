1a1fc51e3777f2a528dc91c5988d6088b1d5207b
Carlo De Franchis <carlodef@gmail.com>
2015-03-01 17:09:07 +0100

    fixed filetype of the cost map written by sgbm. It's now a tif file, instead of a Netpbm PGM image text.

--- python/block_matching.py
+++ python/block_matching.py
@@ -84,7 +84,7 @@ def compute_disparity_map(im1, im2, out_disp, out_mask, algo, disp_min, disp_max
 
     if (algo == 'sgbm'):
         bm_binary = sgbm
-        out_cost = common.tmpfile('tif')
+        out_cost = common.tmpfile('.tif')
         common.run("%s %s %s %s %s %s %d %d %s" %(bm_binary, im1, im2, out_disp,
             out_cost, out_mask, disp_min, disp_max, extra_params))
 
