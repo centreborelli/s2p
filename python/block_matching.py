0f1a6b2250aa6fc4882ed3a8a38f44cfffc56a3b
Carlo De Franchis <carlodef@gmail.com>
2016-02-23 12:52:49 +0100

    cleanup: removed python script to process pinhole images (std) and ipython notebooks

--- python/block_matching.py
+++ python/block_matching.py
@@ -114,7 +114,7 @@ def compute_disparity_map(im1, im2, out_disp, out_mask, algo, disp_min, disp_max
         os.environ['PATH'] = os.environ['PATH'] + os.pathsep + micmac_bin
 
         # prepare micmac xml params file
-        micmac_params = os.path.join(s2p_dir, 'micmac_params.xml')
+        micmac_params = os.path.join(s2p_dir, 'data', 'micmac_params.xml')
         work_dir = os.path.dirname(os.path.abspath(im1))
         common.run('cp %s %s' % (micmac_params, work_dir))
 
