2633647b5ba3eb3888379de73f735e661f26e442
Christophe Palmann <christophe.palmann@c-s.fr>
2016-06-01 14:08:52 +0000

    set default behaviour to average (same method as plyflatten)

--- s2p.py
+++ s2p.py
@@ -271,7 +271,7 @@ def compute_dsm(args):
     flags['min']=3
     flags['max']=4
     flags['median']=5
-    flag = "-flag %d" % ( flags.get(cfg['dsm_option'],5) )
+    flag = "-flag %d" % ( flags.get(cfg['dsm_option'],0) )
     
     if (ymax <= global_ymax):
         common.run("plytodsm %s %f %s %s %f %f %f %f" % ( 
