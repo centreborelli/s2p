09dc4ba292a7e0eb563c7f092c6bcb15451b0242
Carlo de Franchis <carlodef@gmail.com>
2017-01-16 11:09:31 +0100

    [ransac] no ransac if there're not enough points to estimate a model

--- python/sift.py
+++ python/sift.py
@@ -76,13 +76,14 @@ def keypoints_match(k1, k2, method='relative', sift_thresh=0.6, F=None,
         cmd = "%s -f \"%s\"" % (cmd, fij)
     common.run(cmd)
 
-    if os.stat(mfile).st_size > 0:  # filter outliers with ransac
-        if model == 'fundamental':
+    matches = np.loadtxt(mfile)
+    if matches.ndim == 2:  # filter outliers with ransac
+        if model == 'fundamental' and len(matches) >= 7:
             common.run("ransac fmn 1000 .3 7 %s < %s" % (mfile, mfile))
-        if model is 'homography':
+        elif model == 'homography' and len(matches) >= 4:
             common.run("ransac hom 1000 1 4 /dev/null /dev/null %s < %s" % (mfile,
                                                                             mfile))
-        if model is 'hom_fund':
+        elif model == 'hom_fund' and len(matches) >= 7:
             common.run("ransac hom 1000 2 4 /dev/null /dev/null %s < %s" % (mfile,
                                                                             mfile))
             common.run("ransac fmn 1000 .2 7 %s < %s" % (mfile, mfile))
