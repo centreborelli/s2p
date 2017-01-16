63bb17d2bf193f1779174033e420faa0030e0d8b
Carlo de Franchis <carlodef@gmail.com>
2017-01-16 20:35:26 +0100

    less strict testing

--- s2p_test.py
+++ s2p_test.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+
+# s2p (Satellite Stereo Pipeline) testing module
+# Copyright (C) 2017, Carlo de Franchis <carlo.de-franchis@polytechnique.org>
+
+from __future__ import print_function
 import tifffile
 import numpy as np
 
@@ -6,4 +12,16 @@ import s2p
 s2p.main('test.json')
 computed = tifffile.imread('test/dsm.tif')
 expected = tifffile.imread('testdata/expected_output/dsm.tif')
-np.testing.assert_allclose(computed, expected, atol=.5)
+
+# compare shapes
+np.testing.assert_equal(computed.shape, expected.shape)
+
+# compare number of valid pixels
+n_computed = np.count_nonzero(np.isfinite(computed))
+n_expected = np.count_nonzero(np.isfinite(expected))
+np.testing.assert_allclose(n_computed, n_expected, rtol=.01, atol=100)
+
+# check largest difference
+print('99th percentile abs difference', 
+      np.nanpercentile(np.abs(computed - expected), 99))
+assert(np.nanpercentile(np.abs(computed - expected), 99) < 1)
