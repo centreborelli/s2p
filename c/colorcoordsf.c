10c4219696903e9aad09e8fc873b6201313b93b1
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2017-03-07 10:00:16 +0100

    [refactor] new imscript stuff req. for tvl1call.sh

--- c/colorcoordsf.c
+++ c/colorcoordsf.c
@@ -1,21 +1,21 @@
-#ifndef _COLORCOORDS_C
-#define _COLORCOORDS_C
+#ifndef COLORCOORDSF_C
+#define COLORCOORDSF_C
 
 
 #ifndef BAD_MIN
 #define BAD_MIN(a,b) (b)<(a)?(b):(a)
 #endif
 
-static void hsv_to_rgb_doubles(double *out, double *in)
+static void hsv_to_rgb_floats(float *out, float *in)
 {
 	//assert_hsv(in);
-	double r, g, b, h, s, v; r=g=b=h=s=v=0;
+	float r, g, b, h, s, v; r=g=b=h=s=v=0;
 	h = in[0]; s = in[1]; v = in[2];
 	if (s == 0)
 		r = g = b = v;
 	else {
 		int H = fmod(floor(h/60),6);
-		double p, q, t, f = h/60 - H;
+		float p, q, t, f = h/60 - H;
 		p = v * (1 - s);
 		q = v * (1 - f*s);
 		t = v * (1 - (1 - f)*s);
@@ -38,10 +38,10 @@ static void hsv_to_rgb_doubles(double *out, double *in)
 }
 
 
-static void rgb_to_hsv_doubles(double *out, double *in)
+static void rgb_to_hsv_floats(float *out, float *in)
 {
 	//assert_rgb(in);
-	double r, g, b, h, s, v, M, m;
+	float r, g, b, h, s, v, M, m;
 	r = in[0]; g = in[1]; b = in[2];
 
 	//printf("rgb %g,%g,%g...\n", r, g, b);
@@ -76,4 +76,22 @@ static void rgb_to_hsv_doubles(double *out, double *in)
 	out[0] = h; out[1] = s; out[2] = v;
 	//assert_hsv(out);
 }
-#endif//_COLORCOORDS_C
+
+// Commission Intérnationale de l'Éclairage 1931
+static void rgb_to_xyz_floats(float *xyz, float *rgb)
+{
+	float n = 0.17697;
+	xyz[0] = (0.49    * rgb[0] + 0.31   * rgb[1] + 0.2     * rgb[2]) / n;
+	xyz[1] = (0.17697 * rgb[0] + 0.8124 * rgb[1] + 0.01063 * rgb[2]) / n;
+	xyz[2] = (0       * rgb[0] + 0.01   * rgb[1] + 0.99    * rgb[2]) / n;
+}
+
+// Commission Intérnationale de l'Éclairage 1931
+static void xyz_to_rgb_floats(float *rgb, float *xyz)
+{
+	rgb[0] =  0.41847 * xyz[0]    - 0.15866 * xyz[1]   - 0.082835 * xyz[2];
+	rgb[1] = -0.091169 * xyz[0]   + 0.25243 * xyz[1]   + 0.015708 * xyz[2];
+	rgb[2] =  0.00092090 * xyz[0] - 0.0025498 * xyz[1] + 0.1786 * xyz[2];
+}
+
+#endif//COLORCOORDSF_C
