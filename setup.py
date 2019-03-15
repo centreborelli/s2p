f8d555fe8bfaeedffeef0d26ad7b32f7be17f3c5
Carlo de Franchis <carlodef@gmail.com>
2019-03-15 15:05:22 +0100

    [bugfix] update makefile after merge of pull request #193

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ requirements = ['numpy',
                 'requests']
 
 setup(name="s2p",
-      version="1.0b3",
+      version="1.0b4",
       description="Satellite Stereo Pipeline.",
       long_description=readme(),
       url='https://github.com/miss3d/s2p',
