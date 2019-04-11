bb5c5c7a6f3cece837f44c078b5e609a4bca67e2
Carlo de Franchis <carlodef@gmail.com>
2019-04-11 16:24:59 +0200

    [refactor] read nb_points and nb_extra_columns from the input array shape

--- s2p/rasterization.py
+++ s2p/rasterization.py
@@ -17,8 +17,6 @@ parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 lib = ctypes.CDLL(os.path.join(parent_dir, 'lib', 'libplyflatten.so'))
 
 def plyflatten(cloud,
-               nb_points,
-               nb_extra_columns,
                xoff, yoff,
                resolution,
                xsize, ysize,
@@ -28,14 +26,12 @@ def plyflatten(cloud,
 
     Args:
         cloud: A nb_points x (2+nb_extra_columns) numpy array:
-        | x0 y0 [z0 r0 g0 b0 ...] |
-        | x1 y1 [z1 r1 g1 b1 ...] |
-        | ...                     |
-        | xN yN [zN rN gN bN ...] |
-        nb_points: number of points to project
-        nb_extra_columns: number of columns (all columns except x, y): x, y give
-        positions of the points into the final raster, the "extra columns" give the
-        values
+            | x0 y0 [z0 r0 g0 b0 ...] |
+            | x1 y1 [z1 r1 g1 b1 ...] |
+            | ...                     |
+            | xN yN [zN rN gN bN ...] |
+            x, y give positions of the points into the final raster, the "extra
+            columns" give the values
         xoff, yoff: offset position (upper left corner) considering the georeferenced image
         resolution: resolution of the output georeferenced image
         xsize, ysize: size of the georeferenced image
@@ -57,13 +53,14 @@ def plyflatten(cloud,
     lib.rasterize_cloud.restype = ctypes.POINTER(ctypes.c_float)
 
     # Call rasterize_cloud function from libplyflatten.so
+    nb_points, nb_extra_columns = cloud.shape[0], cloud.shape[1] - 2
     raster_ptr = lib.rasterize_cloud(cloud.astype(np.float64),
-                                        nb_points,
-                                        nb_extra_columns,
-                                        xoff, yoff,
-                                        resolution,
-                                        xsize, ysize,
-                                        radius, sigma)
+                                     nb_points,
+                                     nb_extra_columns,
+                                     xoff, yoff,
+                                     resolution,
+                                     xsize, ysize,
+                                     radius, sigma)
 
     # Transform result into a numpy array
     raster = np.asarray([raster_ptr[i] for i in range(nb_extra_columns*xsize*ysize)])
@@ -75,13 +72,16 @@ def plyflatten(cloud,
 
     return raster
 
+
 def plyflatten_from_plyfiles_list(clouds_list, resolution, radius=0, roi=None, sigma=None):
     """
     Projects a points cloud into the raster band(s) of a raster image (points clouds as files)
+
     Args:
         clouds_list: list of cloud.ply files
         resolution: resolution of the georeferenced output raster file
         roi: region of interest: (xoff, yoff, xsize, ysize), compute plyextrema if None
+
     Returns:
         raster: georeferenced raster
         profile: profile for rasterio
@@ -95,8 +95,8 @@ def plyflatten_from_plyfiles_list(clouds_list, resolution, radius=0, roi=None, s
         utm_zone = [comment.split(proj)[-1] for comment in plydata.comments \
                     if proj in comment][0].split()[-1]
 
-        # nb_extra_columns: z, r, b, g (all columns except x, y)
-        nb_extra_columns = len(cloud_data.dtype)-2
+        # nb_extra_columns: z, r, g, b (all columns except x, y)
+        nb_extra_columns = len(cloud_data.dtype) - 2
         full_cloud += [np.array([cloud_data[el] for el in cloud_data.dtype.names]).astype(np.float64).T]
 
     full_cloud = np.concatenate(full_cloud)
@@ -106,8 +106,8 @@ def plyflatten_from_plyfiles_list(clouds_list, resolution, radius=0, roi=None, s
     if roi is not None:
         xoff, yoff, xsize, ysize = roi
     else:
-        xx = full_cloud[:,0]
-        yy = full_cloud[:,1]
+        xx = full_cloud[:, 0]
+        yy = full_cloud[:, 1]
         xmin = np.amin(xx)
         xmax = np.amax(xx)
         ymin = np.amin(yy)
@@ -121,8 +121,7 @@ def plyflatten_from_plyfiles_list(clouds_list, resolution, radius=0, roi=None, s
     # The copy() method will reorder to C-contiguous order by default:
     full_cloud = full_cloud.copy()
     sigma = float("inf")  if sigma is None else sigma
-    raster = plyflatten(full_cloud, nb_points, nb_extra_columns,
-                        xoff, yoff, resolution,
+    raster = plyflatten(full_cloud, xoff, yoff, resolution,
                         xsize, ysize,
                         radius, sigma)
 
