c488902ffde2ccb105aff48a1caa8d4124890665
Guillaume Lostis <g.lostis@kayrros.com>
2019-06-07 11:24:05 +0200

    in-memory plyflatten test

--- tests/common_test.py
+++ tests/common_test.py
@@ -1,21 +1,33 @@
 # s2p (Satellite Stereo Pipeline) testing module
 # Copyright (C) 2019, Julien Michel (CNES) <julien.michel@cnes.fr>
 
-import os
+import numpy as np
+import rasterio
 
 import s2p
 from tests_utils import data_path
 
 
 def test_plyflatten():
-    f = data_path("input_ply/cloud.ply")                       # input cloud
-    e = data_path("expected_output/plyflatten/dsm_40cm.tiff")  # expected output
-    o = s2p.common.tmpfile(".tiff")                       # actual output
-    raster, profile = s2p.rasterization.plyflatten_from_plyfiles_list([f], resolution=0.4) # compute dsm
-    s2p.common.rasterio_write(o, raster[:,:,0], profile=profile) # write dsm
-    s = "\"%w %h %v %Y\n\"" # statistics to compare: width, height, avg, numnans
-    X = s2p.common.tmpfile(".txt")
-    Y = s2p.common.tmpfile(".txt")
-    s2p.common.run("imprintf %s %s > %s" % (s, o, X))     # actual stats
-    s2p.common.run("imprintf %s %s > %s" % (s, e, Y))     # expected stats
-    s2p.common.run("diff %s %s" % (X, Y)) # compare stats
+    # Test data
+    f = data_path("input_ply/cloud.ply")
+    raster, profile = s2p.rasterization.plyflatten_from_plyfiles_list([f], resolution=0.4)
+    test_raster = raster[:, :, 0]  # keep only band with height
+
+    # Expected data
+    e = data_path("expected_output/plyflatten/dsm_40cm.tiff")
+    with rasterio.open(e) as src:
+        expected_raster = src.read(1)
+        expected_crs = src.crs
+        expected_transform = src.transform
+
+    # Check that both rasters are equal pixel-wise within a tolerance
+    assert np.allclose(test_raster, expected_raster, equal_nan=True)
+
+    # Check that both images have the same CRS
+    test_crs = rasterio.crs.CRS.from_proj4(profile['crs'])
+    assert test_crs == expected_crs
+
+    # Check that both images have the same transform
+    test_transform = profile['transform']
+    assert np.allclose(test_transform, expected_transform)
