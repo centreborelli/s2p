fca90da0fe7adfe814921f3e4a672022f120d391
Carlo de Franchis <carlodef@gmail.com>
2019-11-04 11:25:11 +0100

    update version number to 1.0b19dev

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ requirements = ['numpy',
                 'requests']
 
 setup(name="s2p",
-      version="1.0b19",
+      version="1.0b19dev",
       description="Satellite Stereo Pipeline.",
       long_description=readme(),
       long_description_content_type='text/markdown',
