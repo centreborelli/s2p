153182907160bdd1ffec18118405e057181b687c
David de Meij <daviddemeij@gmail.com>
2022-03-22 11:38:52 +0100

    Add --start_from argument to CLI and to s2p.main that allows restarting s2p from a specified step number.

--- s2p/cli.py
+++ s2p/cli.py
@@ -15,11 +15,14 @@ def main():
                         help=('path to a json file containing the paths to '
                               'input and output files and the algorithm '
                               'parameters'))
+    parser.add_argument('--start_from', dest='start_from', type=int,
+                        default=0, help="Integer that indicates the step number from which to "
+                                        "restart the s2p process.")
     args = parser.parse_args()
 
     user_cfg = s2p.read_config_file(args.config)
 
-    s2p.main(user_cfg)
+    s2p.main(user_cfg, start_from=args.start_from)
 
     # Backup input file for sanity check
     if not args.config.startswith(os.path.abspath(s2p.cfg['out_dir']+os.sep)):
