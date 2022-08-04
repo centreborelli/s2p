06d7e46e3868fd328bd5c7ae1bef8f533ec86ab3
lai <helloworld@noreply.com>
2022-08-04 09:18:02 +0200

    Update pyproj to >3.0.0 and reformat setup.py with PEP-8 guide.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import subprocess
 from codecs import open
-from setuptools import setup, find_packages
-from setuptools.command import develop, build_py
+
+from setuptools import find_packages, setup
+from setuptools.command import build_py, develop
 
 
 def readme():
@@ -13,6 +14,7 @@ class CustomDevelop(develop.develop, object):
     """
     Class needed for "pip install -e ."
     """
+
     def run(self):
         subprocess.check_call("make", shell=True)
         super(CustomDevelop, self).run()
@@ -22,6 +24,7 @@ class CustomBuildPy(build_py.build_py, object):
     """
     Class needed for "pip install s2p"
     """
+
     def run(self):
         super(CustomBuildPy, self).run()
         subprocess.check_call("make", shell=True)
@@ -30,10 +33,12 @@ class CustomBuildPy(build_py.build_py, object):
 
 try:
     from wheel.bdist_wheel import bdist_wheel
+
     class BdistWheel(bdist_wheel):
         """
         Class needed to build platform dependent binary wheels
         """
+
         def finalize_options(self):
             bdist_wheel.finalize_options(self)
             self.root_is_pure = False
@@ -42,37 +47,43 @@ except ImportError:
     BdistWheel = None
 
 
-requirements = ['numpy',
-                'scipy',
-                'rasterio[s3]>=1.2a1',
-                'utm',
-                'pyproj>=2.0.2,<3.0.0',
-                'beautifulsoup4[lxml]',
-                'plyfile',
-                'plyflatten>=0.2.0',
-                'ransac',
-                'rpcm>=1.4.6',
-                'srtm4>=1.1.2',
-                'requests']
+requirements = [
+    "numpy",
+    "scipy",
+    "rasterio[s3]>=1.2a1",
+    "utm",
+    "pyproj>3.0.0",
+    "beautifulsoup4[lxml]",
+    "plyfile",
+    "plyflatten>=0.2.0",
+    "ransac",
+    "rpcm>=1.4.6",
+    "srtm4>=1.1.2",
+    "requests",
+]
 
 extras_require = {
     "test": ["pytest", "pytest-cov", "psutil"],
 }
 
-setup(name="s2p",
-      version="1.0b26dev",
-      description="Satellite Stereo Pipeline.",
-      long_description=readme(),
-      long_description_content_type='text/markdown',
-      url='https://github.com/cmla/s2p',
-      packages=['s2p'],
-      install_requires=requirements,
-      extras_require=extras_require,
-      cmdclass={'develop': CustomDevelop,
-                'build_py': CustomBuildPy,
-                'bdist_wheel': BdistWheel},
-      python_requires=">=3",
-      entry_points="""
+setup(
+    name="s2p",
+    version="1.0b26dev",
+    description="Satellite Stereo Pipeline.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/cmla/s2p",
+    packages=["s2p"],
+    install_requires=requirements,
+    extras_require=extras_require,
+    cmdclass={
+        "develop": CustomDevelop,
+        "build_py": CustomBuildPy,
+        "bdist_wheel": BdistWheel,
+    },
+    python_requires=">=3",
+    entry_points="""
           [console_scripts]
           s2p=s2p.cli:main
-      """)
+      """,
+)
