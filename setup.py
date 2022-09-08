e6cae7abdeed9c103b3d32f4a28462b18e96e5b2
David de Meij <daviddemeij@gmail.com>
2022-09-08 17:41:39 +0200

    bump to v1.3.2

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ extras_require = {
 }
 
 setup(name="s2p",
-      version="1.3.1",
+      version="1.3.2",
       description="Satellite Stereo Pipeline.",
       long_description=readme(),
       long_description_content_type='text/markdown',
